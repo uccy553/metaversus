a021eb595a03a93e915e4e52e4201a465463f746
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-09 11:31:27 +0100

    Update CustomTexts.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -5,7 +5,10 @@ import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
-  <motion.p variants={textContainer} className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
         {letter === ' ' ? '\u00A0' : letter}
@@ -15,7 +18,7 @@ export const TypingText = ({ title, textStyles }) => (
 );
 
 export const TitleText = ({ title, textStyles }) => (
- <motion.h2
+  <motion.h2
     variants={textVariant2}
     initial="hidden"
     whileInView="show"
