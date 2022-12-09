7ae9dd8ec808770f62b6ab5c068d69a104f999e3
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-09 11:11:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Page = () => (
     <Hero />
     <div className="relative">
       <About />
-        <div className="gradient-03 z-0"/>
+        <div className="gradient-03 z-0" />
       <Explore />
     </div>
     <div className="relative">
