e5aaaf62101fe898681f26d2bd9d15ac414b599e
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-09 11:05:17 +0100

    Update InsightCard.jsx

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -5,8 +5,8 @@ import { fadeIn } from '../utils/motion';
 
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
-    variants={fadeIn("up", "spring", index * 0.5, 1)}
-    className='flex md:flex-row flex-col gap-4'
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
   >
     <img src={imgUrl} alt="planet" className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover" />
     <div className="w-full flex justify-between items-center">
