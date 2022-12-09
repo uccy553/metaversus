3658b4700eac70b0c1a7a7e5cd62eb4725ef40bc
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-09 11:30:40 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className="bg-primary-black overflow-hidden">
+   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
     <div className="relative">
       <About />
-        <div className="gradient-03 z-0" />
+      <div className="gradient-03 z-0" />
       <Explore />
     </div>
     <div className="relative">
