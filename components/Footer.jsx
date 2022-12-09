b15c5250ef770af29b719d6c110b692c58c545dc
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-09 11:21:08 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => (
       </div>
 
       <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10"/>
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
           <p className="font-normal text-[14px] text-white opacity-50 ">Copyright © 2021 - 2022 Metaversus. All rights reserved.</p>
